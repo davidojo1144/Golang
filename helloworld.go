84b13de1ac065e8e9a22aa72b27d69a871ef4744
davidojo1144 <davco1144@gmail.com>
2024-11-09 12:27:04 +0100

    First Hello World on go

---
+++ helloworld.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt";
+
+func main(){
+    var word string = "hello world"
+    var number int = 2
+    var sure bool = true
+    var num1 float32 = 19.2
+    var num2 float32 = 78.2
+
+    var answer = num1 + num2
+    fmt.Println(answer)
+
+    fmt.Println(word)
+    fmt.Println(number)
+    fmt.Println(sure)
+}
